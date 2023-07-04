e4bd0f82ea026c884d3c8bb2cbf13b961af7954e
Bocchi <22019575@myrp.edu.sg>
2023-07-04 23:52:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22019575/demodatabase/MainActivity.java
@@ -0,0 +1,84 @@
+package sg.edu.rp.c346.id22019575.demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnGetTasks;
+    TextView tvResults;
+    EditText etTask, etDate;
+    ListView lv;
+    ArrayAdapter<Task> adapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks = findViewById(R.id.btnGetTasks);
+        tvResults = findViewById(R.id.tvResults);
+        lv = findViewById(R.id.lv);
+
+
+
+
+
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+
+                ArrayList<Task> data2 = db.getTasks();
+                db.close();
+
+                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data2);
+                lv.setAdapter(adapter);
+
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+
+            }
+        });
+
+
+    }
+
+}
+
